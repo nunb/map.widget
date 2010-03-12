c9ccbb5159e112c7a63a7a1ec5838748a0599d93
Nandan Bagchee <nandan@bagchee.org>
2010-03-12 10:43:22 +0100

    Add tagging

---
+++ src/model/tagged-item.lisp
@@ -0,0 +1,30 @@
+(in-package :app)
+
+;; A mixin that stores tags as keywords
+
+(defparameter *all-tags* (make-hash-table))
+
+(defun add-tag (tag)
+  (sif (gethash tag *all-tags*)
+       (incf it)
+       (setf it 1)))
+
+(defun remove-tag (tag)
+  (sif (gethash tag *all-tags*)
+       (and (> it 0) (decf it))
+       (setf it 0)))
+
+(defun any->keyword (tag)
+  )
+
+(defclass* tagged-item-mixin ()
+  ((tagged-with :initform (make-hash-table))))
+
+(defmethod has-tag ((tim tagged-item-mixin) tag))
+
+(defmethod add-tag ((tim tagged-item-mixin) tag))
+
+(defmethod del-tag ((tim tagged-item-mixin) tag))
+
+(defmethod all-tag ((tim tagged-item-mixin))
+  ())
